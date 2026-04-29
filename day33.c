8d335da8df2028a391c2acc4c4f7d57d4383335a
divyanshigaur2504 <divyanshigaur108@gmail.com>
2026-04-29 12:41:12 +0530

    Add infix to postfix conversion functionality
    Implement infix to postfix conversion using a stack.

--- day33.c
+++ day33.c
@@ -1,1 +1,79 @@
-alr
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+// Push
+void push(char x) {
+    stack[++top] = x;
+}
+
+// Pop
+char pop() {
+    if (top == -1) return -1;
+    return stack[top--];
+}
+
+// Peek
+char peek() {
+    if (top == -1) return -1;
+    return stack[top];
+}
+
+// Precedence
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int j = 0;
+
+    scanf("%s", infix);
+
+    for (int i = 0; infix[i] != '\0'; i++) {
+
+        // Operand
+        if (isalnum(infix[i])) {
+            postfix[j++] = infix[i];
+        }
+
+        // Left parenthesis
+        else if (infix[i] == '(') {
+            push(infix[i]);
+        }
+
+        // Right parenthesis
+        else if (infix[i] == ')') {
+            while (peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); // remove '('
+        }
+
+        // Operator
+        else {
+            while (top != -1 && precedence(peek()) >= precedence(infix[i])) {
+                postfix[j++] = pop();
+            }
+            push(infix[i]);
+        }
+    }
+
+    // Pop remaining operators
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("%s", postfix);
+
+    return 0;
+}
