09d22da9e7d75d0ab75c0116d8ca948b0e01d9db
divyanshigaur2504 <divyanshigaur108@gmail.com>
2026-04-29 12:39:57 +0530

    Implement stack operations in C

--- day31.c
+++ day31.c
@@ -1,1 +1,57 @@
-okkkkkkkkkkkkkkkkkkkk
+#include <stdio.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push
+void push(int x) {
+    if (top == MAX - 1) {
+        // Overflow (not required in question, but safe)
+        return;
+    }
+    stack[++top] = x;
+}
+
+// Pop
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("%d\n", stack[top--]);
+}
+
+// Display (top to bottom)
+void display() {
+    if (top == -1) {
+        return;
+    }
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, type, value;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &type);
+
+        if (type == 1) {        // push
+            scanf("%d", &value);
+            push(value);
+        } 
+        else if (type == 2) {   // pop
+            pop();
+        } 
+        else if (type == 3) {   // display
+            display();
+        }
+    }
+
+    return 0;
+}
