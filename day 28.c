bca2e12bb651fc3f9950d0d0c0e3d7bf238e6190
divyanshigaur2504 <divyanshigaur108@gmail.com>
2026-04-29 12:37:25 +0530

    Add circular linked list implementation in C
    Implement a circular linked list in C that reads user input and displays the elements.

--- day 28.c
+++ day 28.c
@@ -1,1 +1,49 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+
+    if (n <= 0) return 0;
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    // Create circular linked list
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = x;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Make it circular
+    temp->next = head;
+
+    // Traverse circular linked list
+    struct Node* current = head;
+
+    if (current != NULL) {
+        do {
+            printf("%d ", current->data);
+            current = current->next;
+        } while (current != head);
+    }
+
+    return 0;
+}
