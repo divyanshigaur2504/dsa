989cbc370b54d542dc2815528d3c42e0fa4d81a6
divyanshigaur2504 <divyanshigaur108@gmail.com>
2026-04-29 12:35:14 +0530

    Update day37.c

--- day37.c
+++ day37.c
@@ -1,1 +1,81 @@
-okkkkkkkkkkkkkkkkkkkkk
+#include <stdio.h>
+
+#define MAX 100
+
+int pq[MAX];
+int size = 0;
+
+// Insert element
+void insert(int x) {
+    if (size < MAX) {
+        pq[size++] = x;
+    }
+}
+
+// Find index of minimum element
+int findMinIndex() {
+    if (size == 0) return -1;
+
+    int minIndex = 0;
+    for (int i = 1; i < size; i++) {
+        if (pq[i] < pq[minIndex]) {
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+
+// Delete (remove highest priority)
+void delete() {
+    int minIndex = findMinIndex();
+
+    if (minIndex == -1) {
+        printf("-1\n");
+        return;
+    }
+
+    printf("%d\n", pq[minIndex]);
+
+    // Shift elements
+    for (int i = minIndex; i < size - 1; i++) {
+        pq[i] = pq[i + 1];
+    }
+    size--;
+}
+
+// Peek (view highest priority)
+void peek() {
+    int minIndex = findMinIndex();
+
+    if (minIndex == -1) {
+        printf("-1\n");
+        return;
+    }
+
+    printf("%d\n", pq[minIndex]);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    char op[10];
+    int x;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%s", op);
+
+        if (op[0] == 'i') { // insert
+            scanf("%d", &x);
+            insert(x);
+        } 
+        else if (op[0] == 'd') { // delete
+            delete();
+        } 
+        else if (op[0] == 'p') { // peek
+            peek();
+        }
+    }
+
+    return 0;
+}
